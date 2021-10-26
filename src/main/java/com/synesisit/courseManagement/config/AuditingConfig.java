08ff4a10f329196766827f6559db8bca2f31351c
Hasnat <smhasnat55@gmail.com>
2021-10-27 04:10:11 +0600

    To enable JPA Auditing created the config class

---
+++ src/main/java/com/synesisit/courseManagement/config/AuditingConfig.java
@@ -0,0 +1,10 @@
+package com.synesisit.courseManagement.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+    // That's all here for now. Will add more auditing configurations later.
+}
